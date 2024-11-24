fd220b13e431c48145c83736ad4aeed90310c711
shahilalgoo <shahil.algoo@gmail.com>
2024-11-24 16:11:58 +0400

    setting up prototype prefabs

---
+++ game/hackathon-game/Assets/Scripts/Helpers.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public static class Helpers
+{
+    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
+
+    public static Vector3 TargetMousePosition(Camera mainCamera, Vector3 cursorPosition, float lookYOffset)
+    {
+        Ray cameraRay = mainCamera.ScreenPointToRay(cursorPosition);
+        Plane groundPlane = new Plane(Vector3.up, new Vector3(0, lookYOffset, 0));
+        float rayLength;
+
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 collidedPoint = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, collidedPoint, Color.blue);
+            return collidedPoint;
+        }
+
+        return Vector3.zero;
+
+    }
+
+}
--- game/hackathon-game/Assets/Scripts/LevelBuilder.cs
+++ game/hackathon-game/Assets/Scripts/LevelBuilder.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject[] levelObjects;
+
+    private int[,] instructions;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instructions = new int[,] {
+            { 0, 0, 0, 2, 4, 1, 0, 0, 0, 6, 2, 0, 0, 0, 1, 5, 1, 0, 2, 7 },
+            { 0, 1, 0, 1, 0, 2, 3, 1, 0, 3, 0, 0, 2, 1, 2, 0, 1, 2, 1, 0 },
+            { 0, 2, 0, 0, 0, 0, 0, 1, 4, 1, 0, 0, 1, 0, 0, 0, 0, 5, 0, 0 },
+            { 0, 1, 4, 2, 1, 0, 0, 0, 0, 2, 4, 1, 5, 0, 1, 3, 1, 2, 6, 0 },
+            { 1, 2, 0, 0, 5, 1, 3, 6, 0, 0, 0, 0, 1, 4, 2, 0, 0, 0, 2, 1 },
+            { 0, 0, 0, 0, 0, 0, 1, 5, 1, 5, 1, 0, 0, 0, 1, 3, 1, 0, 0, 0 },
+            { 0, 1, 4, 2, 0, 0, 0, 0, 2, 0, 1, 3, 1, 0, 0, 0, 2, 1, 0, 8 },
+            { 0, 0, 0, 3, 1, 2, 1, 0, 4, 0, 0, 0, 2, 4, 1, 0, 0, 5, 1, 1 },
+            { 0, 0, 0, 1, 0, 0, 2, 5, 6, 3, 2, 0, 0, 0, 2, 1, 0, 0, 2, 0 },
+        };
+
+        BuildLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    void BuildLevel()
     {
-        
+        // create 2d array for loop
+        for (int i = 0; i < instructions.GetLength(0); i++)
+        {
+            for (int j = 0; j < instructions.GetLength(1); j++)
+            {
+                int objectId = instructions[i, j];
+                if (objectId == 0) continue;
+                Vector3 pos = new Vector3(objectId, 0, 0);
+                GameObject prefab = Resources.Load<GameObject>("Prefabs/" + objectId);
+                Instantiate(prefab, new Vector3(j, 0, i), Quaternion.identity);
+            }
+        }
     }
 }
