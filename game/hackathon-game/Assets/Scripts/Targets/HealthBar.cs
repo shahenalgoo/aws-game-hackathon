8754d0ff42c82f8d571f4d61950c2c45dd70b2c7
shahilalgoo <shahil.algoo@gmail.com>
2024-11-29 22:12:45 +0400

    added health bar for targets

---
+++ game/hackathon-game/Assets/Scripts/Targets/HealthBar.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private Canvas canvas;
+    private Camera mainCamera;
+
+    // Lerp settings
+    private float targetHealth;
+    [SerializeField] private float lerpSpeed = 10f;
+    private bool isLerping = false;
+
+    // Visibility settings
+    [SerializeField] private float hideDelay = 5f;
+    private float lastDamageTime;
+    private bool isVisible = false;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        // Ensure the canvas is set to world space
+        canvas.renderMode = RenderMode.WorldSpace;
+
+        // Set initial scale of the canvas to be small enough to fit your world
+        canvas.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+        // Set fixed rotation of -45 degrees on Y axis
+        canvas.transform.rotation = Quaternion.Euler(0, -45, 0);
+
+        // Hide health bar initially
+        canvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        // Smooth lerp to target health value
+        // Only lerp when damage is taken
+        if (isLerping)
+        {
+            slider.value = Mathf.Lerp(slider.value, targetHealth, Time.deltaTime * lerpSpeed);
+
+            // Stop lerping when we're close enough to the target value
+            if (Mathf.Abs(slider.value - targetHealth) < 0.01f)
+            {
+                slider.value = targetHealth;
+                isLerping = false;
+            }
+        }
+
+        // Check if we should hide the health bar
+        if (isVisible && Time.time - lastDamageTime > hideDelay)
+        {
+            HideHealthBar();
+        }
+    }
+
+    public void UpdateHealthBar(float currentHealth)
+    {
+        // Show health bar if it's not visible
+        if (!isVisible)
+        {
+            ShowHealthBar();
+        }
+
+        // Only start lerping if health has decreased
+        if (currentHealth < slider.value)
+        {
+            isLerping = true;
+            // Update last damage time
+            lastDamageTime = Time.time;
+        }
+        targetHealth = currentHealth;
+    }
+
+    private void ShowHealthBar()
+    {
+        canvas.enabled = true;
+        isVisible = true;
+        lastDamageTime = Time.time;
+    }
+
+    private void HideHealthBar()
+    {
+        canvas.enabled = false;
+        isVisible = false;
+    }
+
+    public void SetMaxHealth(float maxHealth)
+    {
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+        targetHealth = maxHealth;
+    }
+}
--- game/hackathon-game/Assets/Scripts/Targets/TargetController.cs
+++ game/hackathon-game/Assets/Scripts/Targets/TargetController.cs
@@ -3,13 +3,44 @@ using UnityEngine;
 public class TargetController : MonoBehaviour
 {
 
-    [SerializeField] private int health = 100;
-    // Start is called before the first frame update
+    [SerializeField] private int _maxHealth = 100;
+    [SerializeField] private int _currentHealth;
+
+    private HealthBar healthBar;
+
+    // Reference to the health bar prefab
+    [SerializeField] private GameObject healthBarPrefab;
+    // Offset for health bar position
+    [SerializeField] private Vector3 healthBarOffset = new Vector3(0, 2f, 0);
+
+    public void Start()
+    {
+        _currentHealth = _maxHealth;
+
+        // Spawn the health bar
+        CreateHealthBar();
+    }
+
+    private void CreateHealthBar()
+    {
+        // Instantiate the health bar prefab as a child of this object
+        GameObject healthBarObject = Instantiate(healthBarPrefab, transform.position + healthBarOffset, Quaternion.identity, transform);
+
+        // Get the HealthBar component
+        healthBar = healthBarObject.GetComponent<HealthBar>();
+
+        // Set the initial max health
+        healthBar.SetMaxHealth(_maxHealth);
+    }
     public void TakeDamage(int amount)
     {
-        health -= amount;
 
-        if (health <= 0)
+        _currentHealth -= amount;
+
+        // Update the health bar
+        healthBar.UpdateHealthBar(_currentHealth);
+
+        if (_currentHealth <= 0)
         {
             Die();
         }
