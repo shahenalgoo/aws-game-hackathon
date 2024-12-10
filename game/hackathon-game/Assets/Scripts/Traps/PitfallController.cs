d5d5f23af0932b06c5e7f471e87455e82511008d
shahilalgoo <shahil.algoo@gmail.com>
2024-12-10 20:46:13 +0400

    also added qol safety ledges to pitfalls
    fixed array index issue when checking neighbouring cells

--- game/hackathon-game/Assets/Scripts/Traps/PitfallController.cs
+++ game/hackathon-game/Assets/Scripts/Traps/PitfallController.cs
@@ -5,6 +5,29 @@ public class PitfallController : MonoBehaviour
 
     private CharacterController _playerCC;
     [SerializeField] private ParticleSystem _onBoost;
+
+    [SerializeField] private GameObject[] _safetyLedges; // 0 - left, 1 - top, 2 - right, 3 - down
+
+    void Start()
+    {
+        // get grid pos from world pos
+        Vector2Int gridPos = Helpers.GetGridPosition(transform);
+        int[,] grid = LevelBuilder.Instance.Grid;
+
+
+        // check if there's something on the left, activate safety ledge
+        if (gridPos.y > 0 && grid[gridPos.x, gridPos.y - 1] != 0) _safetyLedges[0].SetActive(true);
+
+        // check right
+        if (gridPos.y < grid.GetLength(1) - 1 && grid[gridPos.x, gridPos.y + 1] != 0) _safetyLedges[2].SetActive(true);
+
+        // check top
+        if (gridPos.x > 0 && grid[gridPos.x - 1, gridPos.y] != 0) _safetyLedges[1].SetActive(true);
+
+        // check bottom
+        if (gridPos.x < grid.GetLength(0) - 1 && grid[gridPos.x + 1, gridPos.y] != 0) _safetyLedges[3].SetActive(true);
+
+    }
     void Update()
     {
         if (_playerCC == null) return;
--- game/hackathon-game/Assets/Scripts/Traps/SawBladeFloorController.cs
+++ game/hackathon-game/Assets/Scripts/Traps/SawBladeFloorController.cs
@@ -5,17 +5,24 @@ public class SawBladeFloorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int tileSize = LevelBuilder.Instance.TileSize;
         // get grid pos from world pos
-        Vector2Int gridPos = new Vector2Int(Mathf.RoundToInt(transform.position.x / tileSize), Mathf.RoundToInt(transform.position.z / tileSize));
-
-
+        Vector2Int gridPos = Helpers.GetGridPosition(transform);
         int[,] grid = LevelBuilder.Instance.Grid;
 
         // if both left and right are 0, rotate floor
-        if (grid[gridPos.x, gridPos.y - 1] == 0 && grid[gridPos.x, gridPos.y + 1] == 0)
+        if (CheckWithinColumnRange(gridPos, grid) && grid[gridPos.x, gridPos.y - 1] == 0 && grid[gridPos.x, gridPos.y + 1] == 0)
         {
             transform.Rotate(0, -90f, 0);
         }
+    }
+
+    private bool CheckWithinColumnRange(Vector2Int gridPos, int[,] grid)
+    {
+        if (gridPos.y == 0 || gridPos.y == grid.GetLength(1) - 1)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
